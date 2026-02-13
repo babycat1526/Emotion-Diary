7279d1fa8a796f4ff330eeb9cae9db3dc55422e1
babycat999 <dhseong999@gmail.com>
2026-02-13 22:59:34 +0900

    페이지 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
 
+// 1. '/' - 모든 일기를 조회하는 Home 페이지
+// 2. '/new' - 새로운 일기를 작성하는 New 페이지
+// 3. '/diary' - 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  return <>hello</>;
+  return (
+    <Routes>
+      {/* Routes 컴퍼넌트 내부엔 Route 컴퍼넌트만 작성할 수 있다. */}
+      {/* Routes 컴퍼넌트 외부의 요소는 페이지 라우팅과 관련없이 모든 페이지에 동일하게 렌더링 된다.*/}
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  );
 }
 
 export default App;
